2eb122c5ee075e7daa0b3cfb9011c6fa2c4eb914
ibnabdullah1 <arfathossen541@gmail.com>
2025-12-15 10:28:59 +0600

    --no-verify

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+const createUser = z.object({
+  password: z.string({
+    required_error: "Password is required",
+  }),
+  user: z.object({
+    name: z.string({
+      required_error: "Name is required!",
+    }),
+    email: z
+      .string({
+        required_error: "Email is required!",
+      })
+      .email("Invalid email format!"),
+    role: z.string({
+      required_error: "Role is required!",
+    }),
+  }),
+});
+
+export const userValidation = {
+  createUser,
+};
