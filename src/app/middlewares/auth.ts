8a5abc5a9706c64aa65eb7f545c728d33cc835f5
ibnabdullah1 <arfathossen541@gmail.com>
2025-12-15 10:29:02 +0600

    --no-verify

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express";
+
+import httpStatus from "http-status";
+import { Secret } from "jsonwebtoken";
+import config from "../../config";
+import ApiError from "../errors/ApiError";
+import { jwtHelpers } from "../helpers/jwtHelpers";
+
+const auth = (...roles: string[]) => {
+  return async (
+    req: Request & { user?: any },
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const token = req.headers.authorization;
+
+      if (!token) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized!");
+      }
+      const verifiedUser = jwtHelpers.verifyToken(
+        token,
+        config.jwt.jwt_secret as Secret
+      );
+
+      req.user = verifiedUser;
+
+      if (roles.length && !roles.includes(verifiedUser.role)) {
+        throw new ApiError(httpStatus.FORBIDDEN, "Forbidden!");
+      }
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+export default auth;
