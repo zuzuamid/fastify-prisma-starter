6d3f46e7ec3b14181476ecc1db3315a1e607852e
ibnabdullah1 <arfathossen541@gmail.com>
2025-12-15 10:29:10 +0600

    --no-verify

---
+++ src/app/helpers/fileUploader.ts
@@ -0,0 +1,46 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import multer from "multer";
+import path from "path";
+import config from "../../config";
+import { ICloudinaryResponse, IFile } from "../interfaces/file";
+
+cloudinary.config({
+  cloud_name: config.cloudinary.cloud_name,
+  api_key: config.cloudinary.api_key,
+  api_secret: config.cloudinary.api_secret,
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(process.cwd(), "uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+export const multerUpload = multer({ storage: storage });
+const uploadToCloudinary = async (
+  file: IFile
+): Promise<ICloudinaryResponse | undefined> => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      file.path,
+      (error: Error, result: ICloudinaryResponse) => {
+        fs.unlinkSync(file.path);
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
+};
+
+export const fileUploader = {
+  upload,
+  uploadToCloudinary,
+};
